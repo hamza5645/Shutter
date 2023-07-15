b25fd07b4ddb073fa66cf3395fd31ed98c8ade00
hamza5645 <hamzaosama5645@outlook>
2023-07-15 15:43:25 +0300

    Initial Commit

---
+++ Shutter/ShutterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShutterApp.swift
+//  Shutter
+//
+//  Created by Hamza Osama on 15/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ShutterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
