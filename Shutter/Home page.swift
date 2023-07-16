95d11a34e5e2075ff14ed4f0489fa5231025bd06
hamza5645 <hamzaosama5645@outlook>
2023-07-16 13:49:17 +0300

    made the third welcome screen, next is to make the skip button work

--- Shutter/Home page.swift
+++ Shutter/Home page.swift
@@ -1,5 +1,5 @@
 //
-//  welcomeThree.swift
+//  Home page.swift
 //  Shutter
 //
 //  Created by Hamza Osama on 16/07/2023.
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct welcomeThree: View {
+struct Home_page: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    welcomeThree()
+    Home_page()
 }
---
+++ Shutter/Lunch and welcome/welcomeThree.swift
@@ -0,0 +1,110 @@
+//
+//  welcomeThree.swift
+//  Shutter
+//
+//  Created by Hamza Osama on 16/07/2023.
+//
+
+import SwiftUI
+
+struct welcomeThree: View {
+    @Environment(\.colorScheme) var colorScheme
+    @State private var isShowingAnotherView = false
+    
+    var body: some View {
+        NavigationView {
+            VStack(alignment: .leading) {
+                HStack {
+                    Spacer()
+                    Button {
+                        //move to the main screen
+                    } label: {
+                        Text("Skip")
+                            .font(.system(size: 14))
+                            .foregroundStyle(Color.primary)
+                            .bold()
+                            .padding(.horizontal)
+                    }
+                }
+                Spacer()
+                
+                Image("IMG3")
+                    .scaledToFit()
+                Spacer()
+                
+                HStack {
+                    Text("Pay")
+                        .font(.system(size: 24))
+                        .fontWeight(.semibold)
+                        .padding(.horizontal)
+                    Spacer()
+                }
+                Spacer()
+                
+                Text("Lorem ipsum dolor sit amet, consectetur adip iscing elitaw. Vivamus nisl augue, blandit sed tempor quis, aliquetquis velas augue. Vivamus sollicitudin eleme ntum rutrum. Cras interdum consectetur blandit tempor nibh.")
+                    .padding(.horizontal)
+                    .foregroundStyle(Color.gray)
+                    .font(.system(size: 16))
+                
+                Spacer()
+                Spacer()
+                Spacer()
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    
+                    Circle()
+                        .frame(width: 10, height: 10)
+                        .padding(.vertical)
+                        .foregroundStyle(Color.secondary)
+                    
+                    Circle()
+                        .frame(width: 10, height: 10)
+                        .padding(.vertical)
+                        .foregroundStyle(Color.secondary)
+                    
+                    Circle()
+                        .frame(width: 10, height: 10)
+                        .padding(.vertical)
+                        .foregroundStyle(Color.primary)
+                    
+                    Spacer()
+                }
+                Spacer()
+                
+                Button {
+                    isShowingAnotherView = true
+                } label: {
+                    HStack {
+                        Spacer()
+                        ZStack {
+                            Rectangle()
+                                .frame(width: 343, height: 48)
+                                .cornerRadius(4)
+                                .padding()
+                                .foregroundStyle(Color.primary)
+                            
+                            Text("Next")
+                                .font(.system(size: 14))
+                                .foregroundStyle(colorScheme == .light ? Color.white : Color.black)
+                        }
+                        Spacer()
+                    }
+                }
+                .background(
+                    NavigationLink (
+                        destination: Home_page()
+                            .navigationBarHidden(true),
+                        isActive: $isShowingAnotherView,
+                        label: { EmptyView() }
+                    )
+                )
+            }
+        }
+    }
+}
+
+#Preview {
+    welcomeThree()
+}
