23682f6385f1d495f803fe27a1474d0b4446244a
hamza5645 <97033430+hamza5645@users.noreply.github.com>
2023-07-21 16:30:14 +0300

    Started Creating the Navigation Bar

---
+++ Shutter/profile.swift
@@ -0,0 +1,18 @@
+//
+//  profile.swift
+//  Shutter
+//
+//  Created by Hamza Osama on 20/07/2023.
+//
+
+import SwiftUI
+
+struct profile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    profile()
+}
---
+++ Shutter/sessions.swift
@@ -0,0 +1,18 @@
+//
+//  sessions.swift
+//  Shutter
+//
+//  Created by Hamza Osama on 20/07/2023.
+//
+
+import SwiftUI
+
+struct sessions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    sessions()
+}
