cc5eaa40cd9ea55cd6f4a983971bc0f43b95237c
hamza5645 <hamzaosama5645@outlook>
2023-07-25 15:21:34 +0300

    Made the logic behind the navigation work and made the code files simpler

--- Shutter/Lunch and welcome/ContentView.swift
+++ Shutter/Lunch and welcome/ContentView.swift
@@ -10,7 +10,6 @@ import Firebase
 
 struct ContentView: View {
     @State private var lunchScreen: Bool = false
-    @State private var userIsLoggedIn = false
     var body: some View {
         ZStack {
             if lunchScreen {
@@ -23,22 +22,6 @@ struct ContentView: View {
             DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
                 withAnimation {
                     self.lunchScreen = true
-                }
-            }
-        }
-    }
-    var content: some View {
-        ZStack {
-            if userIsLoggedIn {
-                sessions()
-            } else {
-                sessions_loggedOut_()
-            }
-        }
-        .onAppear {
-            Auth.auth().addStateDidChangeListener { auth, user in
-                if user != nil {
-                    userIsLoggedIn.toggle()
                 }
             }
         }
--- Shutter/Main Screens/sessions.swift
+++ Shutter/Main Screens/sessions.swift
@@ -6,20 +6,189 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct sessions: View {
     @State private var homeView = false
     @State private var profileView = false
     @State private var loginView = false
     @State private var signupView = false
+    @State private var userIsLoggedIn = false
     
     var body: some View {
+        ZStack {
+            if userIsLoggedIn {
+                content
+            } else {
+                loggedOut
+            }
+        }
+    }
+    
+    var content: some View {
         NavigationView {
             VStack {
                 Spacer()
                 Text("Empty")
                 
                 Spacer()
+                //Nav Bar
+                ZStack {
+                    Rectangle()
+                        .frame(width: 375, height: 50)
+                        .foregroundColor(.white)
+                        .blur(radius: 20)
+                    
+                    HStack {
+                        Spacer()
+                        
+                        Button {
+                            homeView = true
+                        } label: {
+                            VStack {
+                                Image("home")
+                                    .frame(width: 24, height: 24)
+                                
+                                Text("Home")
+                                    .font(.custom("Muli", size: 12))
+                                    .foregroundColor(.black)
+                            }
+                        }
+                        .background(
+                            NavigationLink(
+                                destination: Home_page()
+                                    .navigationBarHidden(true),
+                                isActive: $homeView,
+                                label: { EmptyView() }
+                            )
+                        )
+                        
+                        Spacer()
+                        
+                        
+                        VStack {
+                            Image("sessionsBlack")
+                                .frame(width: 24, height: 24)
+                            
+                            Text("Sessions")
+                                .font(.custom("Muli", size: 12))
+                                .foregroundColor(.black)
+                        }
+                        
+                        Spacer()
+                        
+                        Button {
+                            profileView = true
+                        } label: {
+                            VStack {
+                                Image("profile")
+                                    .frame(width: 24, height: 24)
+                                
+                                Text("Profile")
+                                    .font(.custom("Muli", size: 12))
+                                    .foregroundColor(.black)
+                                
+                                
+                            }
+                            .background(
+                                NavigationLink(
+                                    destination: profile()
+                                        .navigationBarHidden(true),
+                                    isActive: $profileView,
+                                    label: { EmptyView() }
+                                )
+                            )
+                        }
+                        
+                        Spacer()
+                    }
+                }
+            }
+            .onAppear {
+                Auth.auth().addStateDidChangeListener { auth, user in
+                    if user != nil {
+                        userIsLoggedIn.toggle()
+                    }
+                }
+            }
+        }
+    }
+    
+    var loggedOut: some View {
+        NavigationView {
+            VStack{
+                HStack {
+                    Text("Photographers")
+                        .font(.custom("Vollkorn-SemiBold", size: 32))
+                        .padding(.horizontal, 30)
+                        .fontWeight(.semibold)
+                    Spacer()
+                }
+                
+                Divider()
+                
+                
+                
+                Spacer()
+                
+                VStack {
+                    Text("You have to create account login first")
+                        .font(.custom("Muli", size: 16))
+                        .foregroundColor(.secondary)
+                        .padding()
+                    
+                    Button {
+                        signupView = true
+                    } label: {
+                        ZStack {
+                            Rectangle()
+                                .frame(width: 343, height: 48)
+                                .cornerRadius(4)
+                                .foregroundColor(.black)
+                            
+                            HStack {
+                                Text("Create account")
+                                    .foregroundColor(.white)
+                                    .font(.custom("Muli-Bold", size: 14))
+                            }
+                        }
+                    }
+                    .background(
+                        NavigationLink (
+                            destination: signUp()
+                                .navigationBarHidden(true),
+                            isActive: $signupView,
+                            label: { EmptyView() }
+                        )
+                    )
+                    
+                    Button {
+                        loginView = true
+                    } label: {
+                        ZStack {
+                            Rectangle()
+                                .frame(width: 343, height: 48)
+                                .cornerRadius(4)
+                                .foregroundColor(.secondary)
+                            
+                            HStack {
+                                Text("Login")
+                                    .foregroundColor(.black)
+                                    .font(.custom("Muli-Bold", size: 14))
+                            }
+                        }
+                    }
+                    .background(
+                        NavigationLink (
+                            destination: logIn()
+                                .navigationBarHidden(true),
+                            isActive: $loginView,
+                            label: { EmptyView() }
+                        )
+                    )
+                }
+                .padding()
+                
                 //Nav Bar
                 ZStack {
                     Rectangle()
