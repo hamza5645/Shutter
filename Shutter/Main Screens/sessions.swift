96dd6fdb535e610addeedeab1f21b1540ec69768
hamza5645 <hamzaosama5645@outlook>
2023-07-25 15:01:46 +0300

    Created the sessions page if the user is logged in but doesn't have any sessions booked

--- Shutter/Main Screens/sessions.swift
+++ Shutter/Main Screens/sessions.swift
@@ -8,8 +8,91 @@
 import SwiftUI
 
 struct sessions: View {
+    @State private var homeView = false
+    @State private var profileView = false
+    @State private var loginView = false
+    @State private var signupView = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                Spacer()
+                Text("Empty")
+                
+                Spacer()
+                //Nav Bar
+                ZStack {
+                    Rectangle()
+                        .frame(width: 375, height: 50)
+                        .foregroundColor(.white)
+                        .blur(radius: 20)
+                    
+                    HStack {
+                        Spacer()
+                        
+                        Button {
+                            homeView = true
+                        } label: {
+                            VStack {
+                                Image("home")
+                                    .frame(width: 24, height: 24)
+                                
+                                Text("Home")
+                                    .font(.custom("Muli", size: 12))
+                                    .foregroundColor(.black)
+                            }
+                        }
+                        .background(
+                            NavigationLink(
+                                destination: Home_page()
+                                    .navigationBarHidden(true),
+                                isActive: $homeView,
+                                label: { EmptyView() }
+                            )
+                        )
+                        
+                        Spacer()
+                        
+                        
+                        VStack {
+                            Image("sessionsBlack")
+                                .frame(width: 24, height: 24)
+                            
+                            Text("Sessions")
+                                .font(.custom("Muli", size: 12))
+                                .foregroundColor(.black)
+                        }
+                        
+                        Spacer()
+                        
+                        Button {
+                            profileView = true
+                        } label: {
+                            VStack {
+                                Image("profile")
+                                    .frame(width: 24, height: 24)
+                                
+                                Text("Profile")
+                                    .font(.custom("Muli", size: 12))
+                                    .foregroundColor(.black)
+                                
+                                
+                            }
+                            .background(
+                                NavigationLink(
+                                    destination: profile()
+                                        .navigationBarHidden(true),
+                                    isActive: $profileView,
+                                    label: { EmptyView() }
+                                )
+                            )
+                        }
+                        
+                        Spacer()
+                    }
+                }
+            }
+        }
     }
 }
 
